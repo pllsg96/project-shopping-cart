81e9138757f593aa886b376b7215bdc034eb0465
Pedro Luiz <pedroluizlsg@gmail.com>
2022-07-11 17:13:48 -0300

    Verifica se retorna uma função

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -1,8 +1,11 @@
 require('../mocks/fetchSimulator');
 const { fetchItem } = require('../helpers/fetchItem');
+const { fetchProducts } = require('../helpers/fetchProducts');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
   // implemente seus testes aqui  
-  fail('Teste vazio');
+  test('Verify if fetchProducts is a function', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
 });
